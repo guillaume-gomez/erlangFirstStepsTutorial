a133212a9197ce83e9b377fa1c98a8fa81189653
Guillaume Gomez <guillaume.gomez846@gmail.com>
2016-04-22 23:17:19 +0200

    change atom to variable

--- erlangExerciceOreilly/sort.erl
+++ erlangExerciceOreilly/sort.erl
@@ -1,13 +1,14 @@
 -module(sort).
 -compile(export_all).
 
-swap(List, indexElem1, indexElem2) when ((indexElem1 > 1) and (indexElem2 > 1)) ->
-    {List2,[F|List3]} = lists:split(indexElem1-1, List),
-    LT = List2++[lists:nth(indexElem2, List)| List3],
-    {List4,[_|List5]} = lists:split(indexElem2-1, LT),
+swap(List, IndexElem1, IndexElem2) when ((IndexElem1 > 1) and (IndexElem2 > 1)) ->
+    {List2,[F|List3]} = lists:split(IndexElem1-1, List),
+    LT = List2++[lists:nth(IndexElem2, List)| List3],
+    {List4,[_|List5]} = lists:split(IndexElem2-1, LT),
     List4++[F|List5].
 
 %split_loop(List, )
 
 %split(List, First, Last, Pivot) ->
 %    List2 = swap(List, Pivot, Last),
+
